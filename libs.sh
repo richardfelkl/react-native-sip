4ffd0731233f36f8ac2bf51048333a11abfe0e1b
Richard Felkl <richardfelkl@users.noreply.github.com>
2020-11-10 18:29:26 +0100

    Update libs.sh

--- libs.sh
+++ libs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-VERSION="v2.8.0"
-URL="https://github.com/florindumitru/react-native-pjsip-builder/archive/${VERSION}.tar.gz"
+VERSION="v2.8.1"
+URL="https://github.com/richardfelkl/react-native-pjsip-builder/archive/${VERSION}.tar.gz"
 LOCK=".libs.lock"
 DEST=".libs.tar.gz"
 DOWNLOAD=true
@@ -30,8 +30,9 @@ if [ "$DOWNLOAD" = true ]; then
     rm -f "${DEST}"
 
     echo "${VERSION}" > ${LOCK}
-    cd react-native-pjsip-builder-2.8.0
+    cd react-native-pjsip-builder-2.8.1
     ./release.sh
     cp -rf dist/ios/VialerPJSIP.framework ../ios/
     cp -rf dist/android/src/* ../android/src
 fi
+
